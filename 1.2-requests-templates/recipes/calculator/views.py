771e93c3bea87f02ee45636116b30c7d923be124
Lilia Komova <lilak6051@gmail.com>
2023-05-04 10:49:27 +0300

    add modified view.py and urls.py

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -25,42 +25,20 @@ def home_view(request):
     return render(request, template_name)
 
 
-def omlet_view(request):
+def dish_view(request, dish):
+
     template_name = 'calculator/index.html'
+
     servings = int(request.GET.get("servings", 1))
+    for i in DATA.values():
+        for k, v in i.items():
+            i[k] = v * servings
+
     context = {
-        'recipe':
-            {
-                'яйца, шт': 2 * servings,
-                'молоко, л': 0.1 * servings,
-                'соль, ч.л.': 0.5 * servings,
-            }
+        'recipe': DATA[dish]
     }
 
-    return render(request, template_name, context)
-
-
-def pasta_view(request):
-    template_name = 'calculator/index.html'
-    servings = int(request.GET.get("servings", 1))
-    context = {
-        'recipe': {
-            'макароны, г': 0.3 * servings,
-            'сыр, г': 0.05 * servings,
-        },
-    }
-    return render(request, template_name, context)
-
-
-def buter_view(request):
-    template_name = 'calculator/index.html'
-    servings = int(request.GET.get("servings", 1))
-    context = {
-        'recipe': {
-            'хлеб, ломтик': 1,
-            'колбаса, ломтик': 1,
-            'сыр, ломтик': 1,
-            'помидор, ломтик': 1,
-        },
-    }
-    return render(request, template_name, context)
+    if dish in DATA.keys():
+        return render(request, template_name, context)
+    else:
+        return render(request, template_name)
--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -14,15 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path
-from calculator.views import home_view, omlet_view, pasta_view, buter_view
-
+from calculator.views import home_view, dish_view
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('omlet/', omlet_view, name='omlet'),
-    path('pasta/', pasta_view, name='pasta'),
-    path('buter/', buter_view, name='buter'),
-    path('admin/', admin.site.urls)
+    path('<dish>/', dish_view)
 ]
